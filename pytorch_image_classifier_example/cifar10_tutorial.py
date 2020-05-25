08adef4a840b7ded5ddb7e5f109adb79b1b8936c
Tilo Himmelsbach <dertilo@gmail.com>
2020-05-25 09:59:50 +0200

    black formatted

--- pytorch_image_classifier_example/cifar10_tutorial.py
+++ pytorch_image_classifier_example/cifar10_tutorial.py
@@ -14,18 +14,23 @@ size 3x32x32, i.e. 3-channel color images of 32x32 pixels in size.
 
 import os
 import sys
+
 cwd = os.getcwd()
 sys.path.append(cwd)
 
 import torch.nn as nn
 import torch.optim as optim
 from resmonres.monitor_system_parameters import MonitorSysParams
-from pytorch_image_classifier_example.some_methods import train_neural_network, evaluate_model, load_data
+from pytorch_image_classifier_example.some_methods import (
+    train_neural_network,
+    evaluate_model,
+    load_data,
+)
 import torch
 from pytorch_image_classifier_example.neural_nets import SuperCoolNeuralNetwork
 
 
-trainloader,testloader,classes = load_data(cwd)
+trainloader, testloader, classes = load_data(cwd)
 
 # show_images(trainloader,classes)
 net = SuperCoolNeuralNetwork()
@@ -34,13 +39,9 @@ net = SuperCoolNeuralNetwork()
 criterion = nn.CrossEntropyLoss()
 optimizer = optim.SGD(net.parameters(), lr=0.001, momentum=0.9)
 
-
-########################################################################
-# 4. Train the network
-# ^^^^^^^^^^^^^^^^^^^^
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-print('using: %s'%device)
+print("using: %s" % device)
 
-with MonitorSysParams(log_path='.'):
-    train_neural_network(trainloader,net,criterion,optimizer,device)
-    evaluate_model(net,testloader,classes)
+with MonitorSysParams(log_path="."):
+    train_neural_network(trainloader, net, criterion, optimizer, device)
+    evaluate_model(net, testloader, classes)
--- pytorch_image_classifier_example/some_methods.py
+++ pytorch_image_classifier_example/some_methods.py
@@ -6,13 +6,13 @@ import torchvision
 import torchvision.transforms as transforms
 
 
-def show_images(trainloader,classes):
+def show_images(trainloader, classes):
     # functions to show an image
     def imshow(img):
         img = img / 2 + 0.5  # unnormalize
         npimg = img.numpy()
         plt.imshow(np.transpose(npimg, (1, 2, 0)))
-        plt.savefig('./example_images')
+        plt.savefig("./example_images")
         plt.show()
 
     # get some random training images
@@ -21,27 +21,33 @@ def show_images(trainloader,classes):
     # show images
     imshow(torchvision.utils.make_grid(images))
     # print labels
-    print(' '.join('%5s' % classes[labels[j]] for j in range(4)))
+    print(" ".join("%5s" % classes[labels[j]] for j in range(4)))
 
 
 def load_data(path):
     transform = transforms.Compose(
-        [transforms.ToTensor(),
-         transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
-    trainset = torchvision.datasets.CIFAR10(root=path + '/data', train=True,
-                                            download=False, transform=transform)
-    trainloader = torch.utils.data.DataLoader(trainset, batch_size=4,
-                                              shuffle=True, num_workers=2)
-    testset = torchvision.datasets.CIFAR10(root=path + '/data', train=False,
-                                           download=False, transform=transform)
-    testloader = torch.utils.data.DataLoader(testset, batch_size=4,
-                                             shuffle=False, num_workers=2)
-    classes = ('plane', 'car', 'bird', 'cat',
-               'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
+        [transforms.ToTensor(), transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))]
+    )
+    trainset = torchvision.datasets.CIFAR10(
+        root=path + "/data", train=True, download=False, transform=transform
+    )
+    trainloader = torch.utils.data.DataLoader(
+        trainset, batch_size=4, shuffle=True, num_workers=2
+    )
+    testset = torchvision.datasets.CIFAR10(
+        root=path + "/data", train=False, download=False, transform=transform
+    )
+    testloader = torch.utils.data.DataLoader(
+        testset, batch_size=4, shuffle=False, num_workers=2
+    )
+    # fmt: off
+    classes = ('plane', 'car', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
+    # fmt: on
 
-    return trainloader,testloader,classes
+    return trainloader, testloader, classes
 
-def train_neural_network(trainloader,net,criterion,optimizer,device):
+
+def train_neural_network(trainloader, net, criterion, optimizer, device):
     net.to(device)
     for epoch in range(2):  # loop over the dataset multiple times
 
@@ -58,14 +64,13 @@ def train_neural_network(trainloader,net,criterion,optimizer,device):
             # print statistics
             running_loss += loss.item()
             if i % 2000 == 1999:  # print every 2000 mini-batches
-                print('[%d, %5d] loss: %.3f' %
-                      (epoch + 1, i + 1, running_loss / 2000))
+                print("[%d, %5d] loss: %.3f" % (epoch + 1, i + 1, running_loss / 2000))
                 running_loss = 0.0
-    print('Finished Training')
+    print("Finished Training")
 
 
-def evaluate_model(net,testloader,classes):
-    net.to('cpu')
+def evaluate_model(net, testloader, classes):
+    net.to("cpu")
 
     correct = 0
     total = 0
@@ -76,10 +81,12 @@ def evaluate_model(net,testloader,classes):
             _, predicted = torch.max(outputs.data, 1)
             total += labels.size(0)
             correct += (predicted == labels).sum().item()
-    print('Accuracy of the network on the 10000 test images: %d %%' % (
-            100 * correct / total))
-    class_correct = list(0. for i in range(10))
-    class_total = list(0. for i in range(10))
+    print(
+        "Accuracy of the network on the 10000 test images: %d %%"
+        % (100 * correct / total)
+    )
+    class_correct = list(0.0 for i in range(10))
+    class_total = list(0.0 for i in range(10))
     with torch.no_grad():
         for data in testloader:
             images, labels = data
@@ -91,7 +98,7 @@ def evaluate_model(net,testloader,classes):
                 class_correct[label] += c[i].item()
                 class_total[label] += 1
     for i in range(10):
-        print('Accuracy of %5s : %2d %%' % (
-            classes[i], 100 * class_correct[i] / class_total[i]))
-
-
+        print(
+            "Accuracy of %5s : %2d %%"
+            % (classes[i], 100 * class_correct[i] / class_total[i])
+        )
